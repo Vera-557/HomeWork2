04d6f09d95e54037f5dff1f4c820095ca5c09102
Daiquiri <mestelo@inbox.ru>
2024-09-08 15:07:37 +0200

    e

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {public static void main(String[] args) {
+    Ex_1 ex1 = new Ex_1();
+    Ex_2 ex2 = new Ex_2();
+    Ex_3 ex3 = new Ex_3();
+    Ex_4 ex4 = new Ex_4();
+    Ex_5 ex5 = new Ex_5();
+    Ex_6 ex6 = new Ex_6();
+    Ex_6_1 ex_6_1 = new Ex_6_1();
+    Ex_7 ex7 = new Ex_7();
+    Ex_8 ex8 = new Ex_8();
+    Ex_9 ex9 = new Ex_9();
+    Ex_10 ex10 = new Ex_10();
+    }
+
+}
